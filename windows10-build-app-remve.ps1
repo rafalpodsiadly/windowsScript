27c7023f2a5188fa7012bb78604cadf1ec3b7359
Rafał Podsiadły <rafal.podsiadly@alior.pl>
2016-09-24 18:05:57 +0200

    Windows 10 remove Package

--- windows10-build-app-remve.ps1
+++ windows10-build-app-remve.ps1
@@ -1,109 +1,97 @@
 #list all package 
 #Get-AppxPackage -allusers | Select Name, PackageFullName
 
-#uninstall template 
+#uninstall templ
 #Get-AppxPackage PackageFullName | Remove-AppxPackage
 
-$Applist = Get-AppxProvisionedPackagee -oneline
-echo $Applist;
+# Uninstall Appconnector # C:\Users\%USERNAME%\AppData\Local\Packages\Microsoft.Appconnector_SOMETHING
+#Uninstall Cortana: This app can’t be removed.
 
-$apps = @(
-    #Uninstall Cortana: This app can’t be removed.
+Get-AppxPackage -AllUsers "*Appconnector*"|Remove-AppxPackage
 
-    # Uninstall 3D Builder:
-    "*3DBuilder*", 
+# Uninstall 3D Builder
+Get-AppxPackage -AllUsers "*3DBuilder*"|Remove-AppxPackage
 
-    # Uninstall Appconnector # C:\Users\%USERNAME%\AppData\Local\Packages\Microsoft.Appconnector_SOMETHING
-    "*Appconnector*", 
+# Uninstall Calculator
+#Get-AppxPackage -AllUsers "*windowscalculator*"|Remove-AppxPackage
 
-    # Uninstall # Uninstall Calculator:
-#    "windowscalculator", 
+# ninstall Alarms and Clock:
+Get-AppxPackage -AllUsers "*WindowsAlarms*"|Remove-AppxPackage 
 
-    # ninstall Alarms and Clock:
-    "*WindowsAlarms*", 
+# Uninstall Camera:
+Get-AppxPackage -AllUsers "*WindowsCamera*"|Remove-AppxPackage
 
-    # Uninstall Camera:
-    "*WindowsCamera*", 
+# Uninstall Get Skype:
+Get-AppxPackage -AllUsers "*SkypeApp*"|Remove-AppxPackage
 
-    # Uninstall Get Skype:
-    "*SkypeApp*", 
+# Uninstall Get Office:
+#Get-AppxPackage -AllUsers "*MicrosoftOfficeHub*"|Remove-AppxPackage
 
-    # Uninstall Get Office:
-    "*MicrosoftOfficeHub*", 
+# Uninstall Calendar and Mail apps together:
+Get-AppxPackage -AllUsers "*windowscommunicationsapps*"|Remove-AppxPackage 
 
-    # Uninstall Calendar and Mail apps together:
-    "*windowscommunicationsapps*", 
+# Uninstall Get Started:
+Get-AppxPackage -AllUsers "*Getstarted*"|Remove-AppxPackage 
 
-    # Uninstall Get Started:
-    "*Getstarted*", 
+# Uninstall Groove Music:
+Get-AppxPackage -AllUsers "*ZuneMusic*"|Remove-AppxPackage
 
-    # Uninstall Groove Music:
-    "*ZuneMusic*", 
-    
-    # Uninstall Money:
-    "*BingFinance*",
+# Uninstall Money:
+Get-AppxPackage -AllUsers "*BingFinance*"|Remove-AppxPackage
 
-    # Uninstall News:
-    "*BingNews*",
+# Uninstall News:
+Get-AppxPackage -AllUsers "*BingNews*"|Remove-AppxPackage
 
-    # Uninstall Sports:
-    "*BingSports*",
+# Uninstall Sports:
+Get-AppxPackage -AllUsers "*BingSports*"|Remove-AppxPackage
 
-    # Uninstall Weather:
-    "*BingWeather*",
-    # Uninstall Phone:
-    "*CommsPhone*",
+# Uninstall Weather:
+Get-AppxPackage -AllUsers "*BingWeather*"|Remove-AppxPackage
+# Uninstall Phone:
+Get-AppxPackage -AllUsers "*CommsPhone*"|Remove-AppxPackage
 
-    "*ConnectivityStore*",
+Get-AppxPackage -AllUsers "*ConnectivityStore*"|Remove-AppxPackage
 
-    # Uninstall Microsoft Edge: This app can’t be removed.
+# Uninstall Microsoft Edge: This app can’t be removed.
 
-    # Uninstall Windows Feedback: This app can’t be removed.
+# Uninstall Windows Feedback: This app can’t be removed.
 
-    # alternative
-    # Uninstall Messaging and Skype Video apps together:
-    "*Messaging*",
+# alternative
+# Uninstall Messaging and Skype Video apps together:
+Get-AppxPackage -AllUsers "*Messaging*"|Remove-AppxPackage
 
-    # Uninstall Microsoft Solitaire Collection:
-    "*MicrosoftSolitaireCollection*",
+# Uninstall Microsoft Solitaire Collection:
+Get-AppxPackage -AllUsers "*MicrosoftSolitaireCollection*"|Remove-AppxPackage
 
-    # Uninstall OneNote:
-    "*OneNote*",
-    
-    # Uninstall Sway:
-    "*Sway*",
+# Uninstall OneNote:
+Get-AppxPackage -AllUsers "*OneNote*"|Remove-AppxPackage
 
-    # Uninstall People:
-    "*People*",
+# Uninstall Sway:
+Get-AppxPackage -AllUsers "*Sway*"|Remove-AppxPackage
 
-    # Uninstall Maps:
-    "*WindowsMaps*",
+# Uninstall People:
+Get-AppxPackage -AllUsers "*People*"|Remove-AppxPackage
 
-    # Uninstall Phone Companion:
-    "*Phone*",
+# Uninstall Maps:
+Get-AppxPackage -AllUsers "*WindowsMaps*"|Remove-AppxPackage
 
-    # Uninstall Photos:
-    #"*photos*",
+# Uninstall Phone Companion:
+Get-AppxPackage -AllUsers "*Phone*"|Remove-AppxPackage
 
-    # Uninstall Voice Recorder:
-    "*WindowsSoundRecorder*",
+# Uninstall Photos:
+#"*photos*"|Remove-AppxPackage|Remove-AppxPackage
 
-    # Uninstall Windows Store: (Be very careful!)
-    "*WindowsStore*",
+# Uninstall Voice Recorder:
+Get-AppxPackage -AllUsers "*WindowsSoundRecorder*"|Remove-AppxPackage
 
-    # Uninstall Xbox:
-    "*XboxApp*",
+# Uninstall Windows Store: (Be very careful!)
+Get-AppxPackage -AllUsers "*WindowsStore*"|Remove-AppxPackage
 
-    # Uninstall Movies & TV:
-    "*ZuneVideo*",
+# Uninstall Xbox:
+Get-AppxPackage -AllUsers "*XboxApp*"|Remove-AppxPackage
 
-    # Uninstall Groove Music and Movies & TV apps together:
-    "*zune*"
-    
-);
+# Uninstall Movies & TV:
+Get-AppxPackage -AllUsers "*ZuneVideo*"|Remove-AppxPackage
 
-for ($i=0; $i -lt $apps.length; $i++) {
-    $Applist | WHere-Object {$_.packagename -like $apps[$i]}|Remove-AppxPackage
-    $Applist | WHere-Object {$_.packagename -like $apps[$i]}|Remove-AppxProvisionedPackage -online
-
-}
+# Uninstall Groove Music and Movies & TV apps together:
+Get-AppxPackage -AllUsers "*zune*"|Remove-AppxPackage
